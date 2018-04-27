33b011ac08b9c597cd15651398004710658fb800
Tomáš Celizna <tomas.celizna@gmail.com>
2018-04-27 10:13:21 +0200

    improve naming

--- test/dragonfly_puppeteer/generators/screenshot_test.rb
+++ test/dragonfly_puppeteer/generators/screenshot_test.rb
@@ -3,21 +3,21 @@ require 'test_helper'
 describe DragonflyPuppeteer::Generators::Screenshot do
   let(:generator) { DragonflyPuppeteer::Generators::Screenshot.new }
   let(:app) { test_app.configure_with(:puppeteer) }
-  let(:screenshot) { Dragonfly::Content.new(app) }
+  let(:content) { Dragonfly::Content.new(app) }
 
   let(:source) { 'https://www.google.com' }
 
   describe 'formats' do
     describe 'png' do
-      before { generator.call(screenshot, source, 'format' => 'png') }
-      it { get_mime_type(screenshot.path).must_include "image/png" }
-      it { screenshot.meta.must_equal({"format" => "png"}) }
+      before { generator.call(content, source, 'format' => 'png') }
+      it { get_mime_type(content.path).must_include "image/png" }
+      it { content.meta.must_equal({"format" => "png"}) }
     end
 
     describe 'jpg' do
-      before { generator.call(screenshot, source, 'format' => 'jpg') }
-      it { get_mime_type(screenshot.path).must_include "image/jpeg" }
-      it { screenshot.meta.must_equal({"format" => "jpg"}) }
+      before { generator.call(content, source, 'format' => 'jpg') }
+      it { get_mime_type(content.path).must_include "image/jpeg" }
+      it { content.meta.must_equal({"format" => "jpg"}) }
     end
   end
 
@@ -26,24 +26,24 @@ describe DragonflyPuppeteer::Generators::Screenshot do
     let(:height) { 600 }
     let(:viewport_opts) { { width: width, height: height } }
 
-    before { generator.call(screenshot, source, 'viewport_opts' => viewport_opts) }
+    before { generator.call(content, source, 'viewport_opts' => viewport_opts) }
 
     describe 'width' do
-      it { image_properties(screenshot)[:width].must_equal width }
+      it { image_properties(content)[:width].must_equal width }
     end
 
     describe 'height' do
       let(:height) { 200 }
 
-      it { image_properties(screenshot)[:height].must_equal height }
+      it { image_properties(content)[:height].must_equal height }
     end
 
     describe 'deviceScaleFactor' do
       let(:deviceScaleFactor) { 2 }
       let(:viewport_opts) { { width: width, height: height, deviceScaleFactor: deviceScaleFactor } }
 
-      it { image_properties(screenshot)[:width].must_equal width * 2 }
-      it { image_properties(screenshot)[:height].must_equal height * 2 }
+      it { image_properties(content)[:width].must_equal width * 2 }
+      it { image_properties(content)[:height].must_equal height * 2 }
     end
   end
 
@@ -53,13 +53,13 @@ describe DragonflyPuppeteer::Generators::Screenshot do
     let(:viewport_opts) { { width: width } }
     let(:screenshot_opts) { {} }
 
-    before { generator.call(screenshot, source, 'viewport_opts' => viewport_opts, 'screenshot_opts' => screenshot_opts) }
+    before { generator.call(content, source, 'viewport_opts' => viewport_opts, 'screenshot_opts' => screenshot_opts) }
 
     describe 'fullPage' do
       let(:screenshot_opts) { { fullPage: true } }
 
-      it { image_properties(screenshot)[:width].must_be :>, width }
-      it { image_properties(screenshot)[:height].must_be :>, height }
+      it { image_properties(content)[:width].must_be :>, width }
+      it { image_properties(content)[:height].must_be :>, height }
     end
   end
 
@@ -75,9 +75,9 @@ describe DragonflyPuppeteer::Generators::Screenshot do
       </html>
     } }
 
-    before { generator.call(screenshot, source, 'screenshot_opts' => screenshot_opts) }
+    before { generator.call(content, source, 'screenshot_opts' => screenshot_opts) }
 
-    it { image_properties(screenshot)[:height].must_equal actual_height }
+    it { image_properties(content)[:height].must_equal actual_height }
   end
 
   # ---------------------------------------------------------------------
